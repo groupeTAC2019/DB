dd55f70d301ed859dddb3518a3f89cbd3452bf33
corentingoo <corentingoo@gmail.com>
2019-11-05 11:07:43 +0100

    ajout dans la table membre

--- InsertHomeShare.sql
+++ InsertHomeShare.sql
@@ -3,3 +3,10 @@ go
 
 INSERT INTO Pays (nom)
 VALUES ('Belgique'),('France'),('Allemagne'),('Suisse'),('Hollande')
+
+INSERT INTO Membre (nom,prenom,email,tel,is_admin,mdp,id_pays)
+VALUES ('Jean','Charle','jean.charle@gmail.com','012962657',0,'1234', (SELECT id_pays FROM Pays where nom ='France')),
+('Goovaerts','Corentin','corentin.goovaerts@gmail.com','080378420',1,'4321',(SELECT id_pays FROM Pays where nom ='Belgique')),
+('Surin','Thomas','ythomas@gmail.com','04086403',1,'0000',(SELECT id_pays FROM Pays where nom ='Belgique')),
+('Lerusse','Alexandre','alex@gmail.com','065257361',1,'1111',(SELECT id_pays FROM Pays where nom ='Belgique'))
+
