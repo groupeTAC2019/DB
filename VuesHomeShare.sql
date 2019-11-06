99bd2decd71bd4d3d71585fa1851994b8b72e8f0
ToSu <surin.thomas@hotmail.com>
2019-11-06 15:40:28 +0100

    ajout de la vue bien noter > 6

--- VuesHomeShare.sql
+++ VuesHomeShare.sql
@@ -17,3 +17,22 @@ SELECT B.titre AS "titre bien",
 		FROM Bien AS B LEFT JOIN Adresse AS A ON B.id_adresse = A.id_adresse
 					LEFT JOIN Pays AS Pa ON A.id_pays = Pa.id_pays
 GO
+
+CREATE VIEW V_Bien_Bonne_Note
+AS
+SELECT B.titre AS "titre bien",
+		B.desc_courte AS "Description courte",
+		B.desc_longue AS "Description longue",
+		B.disponible AS "Disponible",
+		B.nb_personne AS "Nombre de personne",
+		A.num AS "numero",
+		A.rue AS "rue",
+		A.ville AS "Ville",
+		A.cp AS "code postal",
+		A.boite AS "boite",
+		Pa.nom AS "Pays"
+		FROM Bien AS B LEFT JOIN Adresse AS A ON B.id_adresse = A.id_adresse
+					LEFT JOIN Pays AS Pa ON A.id_pays = Pa.id_pays
+					LEFT JOIN Commentaire AS C ON  B.id_bien = id_commentaire
+WHERE C.note >= 6
+GO
