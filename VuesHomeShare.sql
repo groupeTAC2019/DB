718c194fb14548d3c2de29a5da95d80ff45fe3dd
corentingoo <corentingoo@gmail.com>
2019-11-06 14:40:48 +0100

    création fichier des vues , ajout des bien par pays(V_Bien_Pays)

---
+++ VuesHomeShare.sql
@@ -0,0 +1,15 @@
+CREATE VIEW V_Bien_Pays
+AS
+SELECT B.titre AS "titre bien",
+		B.desc_courte AS "Description courte",
+		B.desc_longue AS "Description longue",
+		B.disponible AS "Disponible",
+		B.nb_personne AS "Nombre de personne",
+		A.num AS "numero",
+		A.rue AS "rue",
+		A.ville AS "Ville",
+		A.cp AS "code postal",
+		A.boite AS "boite",
+		Pa.nom AS "Pays"
+		FROM Bien AS B LEFT JOIN Adresse AS A ON B.id_adresse = A.id_adresse
+					LEFT JOIN Pays AS Pa ON A.id_pays = Pa.id_pays
