e968a437375177791e9d7dc3d2ab8d4ccc26af6a
corentingoo <corentingoo@gmail.com>
2019-11-06 12:19:12 +0100

    ajout procedure Details_Bien

--- ProcedureHomeShare.sql
+++ ProcedureHomeShare.sql
@@ -33,3 +33,27 @@ AS
 SELECT * FROM Bien WHERE id_membre = @id_membre
 GO
 
+CREATE PROCEDURE Details_Bien @id_bien INT
+AS
+SELECT B.id_bien AS "id bien",
+		B.titre AS "Titre du bien",
+		B.desc_courte AS "Desccritpion courte",
+		B.desc_longue AS "Desccritpion longue",
+		B.nb_personne AS "Nombre de personne",
+		B.disponible AS Disponible,
+		B.date_desactivation AS "date de desactivation",
+		A.ville AS Ville,
+		A.cp AS CP,
+		A.rue AS Rue,
+		A.num AS Numero,
+		A.boite AS Boite,
+		Pa.nom AS Pays,
+		M.nom,
+		M.email,
+		Ph.legende,
+		Ph.lien
+	FROM Bien AS B LEFT JOIN Adresse AS A ON B.id_adresse = A.id_adresse
+					LEFT JOIN Pays AS Pa ON A.id_pays = Pa.id_pays
+					LEFT JOIN Membre AS M ON B.id_membre = M.id_membre
+					LEFT JOIN Photo AS Ph ON B.id_bien = Ph.id_bien
+		WHERE B.id_bien = @id_bien
