91a203cc4854d95c7a9c7b595e9fccf5b8e4cb69
corentingoo <corentingoo@gmail.com>
2019-11-08 15:15:11 +0100

    retrait de ardesse et des alter tables

--- TrigersHomeShare/SqlTrigersDb.sql
+++ TrigersHomeShare/SqlTrigersDb.sql
@@ -1,34 +1,12 @@
---CREATE TRIGGER isDelete
---ON Adresse
---INSTEAD OF DELETE
---AS
---begin
---	UPDATE Adresse 
---	set isDelete = 1
---	where id_adresse in (select id_adresse from deleted)
---end
-
---ALTER TABLE Adresse ADD isDelete bit default 0
-
---update Adresse 
---set isDelete = 0
-
---begin transaction
---delete from Adresse where id_adresse = 2
---select * from Adresse
---rollback transaction
-
---CREATE TRIGGER DeleteTrigger
---ON Bien
---INSTEAD OF DELETE
---AS
---begin
---	UPDATE Bien 
---	set isDelete = 1
---	where id_bien in (select id_bien from deleted)
---end
-
---ALTER TABLE Bien ADD isDelete bit default 0
+CREATE TRIGGER DeleteTrigger
+ON Bien
+INSTEAD OF DELETE
+AS
+begin
+	UPDATE Bien 
+	set isDelete = 1
+	where id_bien in (select id_bien from deleted)
+end
 
 --update Bien 
 --set isDelete = 0
@@ -48,12 +26,11 @@ begin
 	where id_membre in (select id_membre from deleted)
 end
 
-ALTER TABLE Membre ADD isDelete bit default 0
 
-update Membre 
-set isDelete = 0
+--update Membre 
+--set isDelete = 0
 
-begin transaction
-delete from Membre where id_membre = 2
-select * from Membre
-rollback transaction
+--begin transaction
+--delete from Membre where id_membre = 2
+--select * from Membre
+--rollback transaction
