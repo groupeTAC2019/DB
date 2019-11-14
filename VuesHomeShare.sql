c33c987c3107d44f5d6b952b61e22897c6df1b46
corentingoo <corentingoo@gmail.com>
2019-11-14 12:31:49 +0100

    FIX vues V_Bien_Pays et V_Bien_Bonne_Note

--- VuesHomeShare.sql
+++ VuesHomeShare.sql
@@ -3,16 +3,10 @@ GO
 
 CREATE VIEW V_Bien_Pays
 AS
-SELECT B.*,
-		A.num,
-		A.rue,
-		A.ville,
-		A.cp,
-		A.boite,
-		A.id_pays,
-		Pa.nom
+SELECT B.*
 		FROM Bien AS B LEFT JOIN Adresse AS A ON B.id_adresse = A.id_adresse
 					LEFT JOIN Pays AS Pa ON A.id_pays = Pa.id_pays
+					ORDER BY Pa.nom
 GO
 
 CREATE VIEW V_Dernier_5_Bien
@@ -26,16 +20,7 @@ GO
 
 CREATE VIEW V_Bien_Bonne_Note
 AS
-SELECT B.*,
-		A.num,
-		A.rue,
-		A.ville,
-		A.cp,
-		A.boite,
-		A.id_pays,
-		Pa.nom
-		FROM Bien AS B LEFT JOIN Adresse AS A ON B.id_adresse = A.id_adresse
-					LEFT JOIN Pays AS Pa ON A.id_pays = Pa.id_pays
-					LEFT JOIN Commentaire AS C ON  B.id_bien = id_commentaire
+SELECT B.*
+		FROM Bien AS B LEFT JOIN Commentaire AS C ON  B.id_bien = id_commentaire
 WHERE C.note >= 6
 GO
